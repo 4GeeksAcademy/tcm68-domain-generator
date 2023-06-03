cce438b5483b45615d0a42c234b40f10ba94af0a
Tcm68 <131905512+Tcm68@users.noreply.github.com>
2023-06-03 00:03:01 +0000

    moved domains app.js into original app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const pronouns = [ "Sailor", "Naruto", "Dragon", "Sakura", "OnePiece", ];
+  
+const adjectives = [ "Eternal","Mystic","Legendary","Magical","Shining",];
+  
+const nouns = [ "Sword", "Spirit", "Hero", "Star", "Champion", ];
+  
+const stones = [ "Ruby", "Emerald", "Sapphire", "Amethyst", "Topaz", ];
+
+const endings = [ ".com", ".net", ".org", ".io", ".co", ];
+  
+ 
+  function generateDomain() {
+    const randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const randomStone = stones[Math.floor(Math.random() * stones.length)];
+    const randomEnd = endings[Math.floor(Math.random() * endings.length)];
+  
+    const domainName = `${randomPronoun}${randomAdjective}${randomNoun}${randomStone}${randomEnd}`;
+    return domainName;
+  }
+
+  function generateMultipleDomains(count) {
+    for (let i = 0; i < count; i++) {
+      const domain = generateDomain();
+      console.log(domain);
+    }
+  }
+
+  generateMultipleDomains(30);
